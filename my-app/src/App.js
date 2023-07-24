096685ca9e8ab63ba334d558ae417ef77442279d
Pterodaktilitsa <pterodaktilitsa@gmail.com>
2023-07-24 10:53:44 +0300

    Products

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,47 @@
-import styles from '.app.module.css';
+import { useState } from 'react';
+import {
+	useRequestAddVacuumCleaner,
+	useRequestDeleteHairDryer,
+	useRequestGetProducts,
+	useRequestUpdateSmartphone,
+} from './hooks';
+import styles from './App.module.css';
 
 export const App = () => {
+	const [refreshProductsFlag, setRefreshProductsFlag] = useState(false);
+	const refreshProducts = () => setRefreshProductsFlag(!refreshProductsFlag);
+
+	const { isLoading, products } = useRequestGetProducts(refreshProductsFlag);
+
+	const { isCreating, requestAddVacuumCleaner } =
+		useRequestAddVacuumCleaner(refreshProducts);
+
+	const { isUpdating, requestUpdateSmartphone } =
+		useRequestUpdateSmartphone(refreshProducts);
+
+	const { isDeliting, requestDeleteHairDryer } =
+		useRequestDeleteHairDryer(refreshProducts);
+
 	return (
 		<div className={styles.app}>
-			<label className={styles.appLabel}>Приложение</label>
-			<div>Разная информация приложения</div>
-			<div className={styles.user}>
-				<label>Пользователь:</label>
-			</div>
+			{isLoading ? (
+				<div className={styles.loader}></div>
+			) : (
+				products.map(({ id, name, price }) => (
+					<div key={id}>
+						{name} - {price} руб
+					</div>
+				))
+			)}
+			<button disabled={isCreating} onClick={requestAddVacuumCleaner}>
+				Добавить пылесос
+			</button>
+			<button disabled={isUpdating} onClick={requestUpdateSmartphone}>
+				Обновить смартфон
+			</button>
+			<button disabled={isDeliting} onClick={requestDeleteHairDryer}>
+				Удалить фен
+			</button>
 		</div>
 	);
 };
